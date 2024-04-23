e61493a19aa6db88b34c93fb70a9d5584dfa9223
UmerHamza <umerhamza73@gmail.com>
2024-04-23 15:34:18 +0500

    initial commit

---
+++ app/src/main/java/com/example/composepermissionflow/PermissionDialog.kt
@@ -0,0 +1,115 @@
+package com.example.composepermissionflow
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PermissionDialog(
+    permissionTextProvider: PermissionTextProvider,
+    isPermanentlyDeclined: Boolean,
+    onDismiss: () -> Unit,
+    onOkClick: () -> Unit,
+    onGoToAppSettingsClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BasicAlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismiss,
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    text = "Permission required"
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = permissionTextProvider.getDescription(
+                        isPermanentlyDeclined = isPermanentlyDeclined
+                    ),
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+
+                HorizontalDivider()
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = if (isPermanentlyDeclined) {
+                        "Grant permission"
+                    } else {
+                        "OK"
+                    },
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            if (isPermanentlyDeclined) {
+                                onGoToAppSettingsClick()
+                            } else {
+                                onOkClick()
+                            }
+                        }
+                )
+            }
+        }
+    )
+}
+
+
+interface PermissionTextProvider {
+    fun getDescription(isPermanentlyDeclined: Boolean): String
+}
+
+class CameraPermissionTextProvider : PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if (isPermanentlyDeclined) {
+            "It seems you permanently declined camera permission. " +
+                    "You can go to the app settings to grant it."
+        } else {
+            "This app needs access to your camera so that your friends " +
+                    "can see you in a call."
+        }
+    }
+}
+
+class RecordAudioPermissionTextProvider : PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if (isPermanentlyDeclined) {
+            "It seems you permanently declined microphone permission. " +
+                    "You can go to the app settings to grant it."
+        } else {
+            "This app needs access to your microphone so that your friends " +
+                    "can hear you in a call."
+        }
+    }
+}
+
+class PhoneCallPermissionTextProvider : PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if (isPermanentlyDeclined) {
+            "It seems you permanently declined phone calling permission. " +
+                    "You can go to the app settings to grant it."
+        } else {
+            "This app needs phone calling permission so that you can talk " +
+                    "to your friends."
+        }
+    }
+}
